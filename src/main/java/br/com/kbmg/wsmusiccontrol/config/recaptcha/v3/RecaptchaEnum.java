575263cc1d668b1df911556e0d486074bce2efa0
Kevin Gomes <kb.developer.br@gmail.com>
2021-10-07 19:18:05 -0400

    recaptchaEnum-> remove values not necessary

--- src/main/java/br/com/kbmg/wsmusiccontrol/config/recaptcha/v3/RecaptchaEnum.java
+++ src/main/java/br/com/kbmg/wsmusiccontrol/config/recaptcha/v3/RecaptchaEnum.java
@@ -3,10 +3,7 @@ package br.com.kbmg.wsmusiccontrol.config.recaptcha.v3;
 import lombok.Getter;
 
 public enum RecaptchaEnum {
-    LOGIN_ACTION("login"),
-    REGISTER_ACTION("register"),
-    REGISTER_CONFIRMATION_ACTION("register-confirmation"),
-    REGISTER_PASSWORD_ACTION("register-password");
+    LOGIN_ACTION("login");
 
     @Getter
     private final String value;
