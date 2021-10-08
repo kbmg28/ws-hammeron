de42ccf467cfc6566f7207484791dd82c65054d9
Kevin Gomes <kevin28bruno@gmail.com>
2021-10-07 22:38:03 -0400

    password validation logic

---
+++ src/main/java/br/com/kbmg/wsmusiccontrol/annotations/impl/PasswordConstraintValidator.java
@@ -0,0 +1,44 @@
+package br.com.kbmg.wsmusiccontrol.annotations.impl;
+
+import br.com.kbmg.wsmusiccontrol.annotations.ValidPassword;
+import com.google.common.base.Joiner;
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.LengthRule;
+import org.passay.PasswordData;
+import org.passay.PasswordValidator;
+import org.passay.RepeatCharacterRegexRule;
+import org.passay.RuleResult;
+import org.passay.WhitespaceRule;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+
+public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
+
+    @Override
+    public void initialize(ValidPassword arg0) {
+    }
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext context) {
+        PasswordValidator validator = new PasswordValidator(Arrays.asList(
+                new CharacterRule(EnglishCharacterData.LowerCase),
+                new CharacterRule(EnglishCharacterData.UpperCase),
+                new CharacterRule(EnglishCharacterData.Digit),
+                new LengthRule(6, 20),
+                new RepeatCharacterRegexRule(3),
+                new WhitespaceRule()));
+
+        RuleResult result = validator.validate(new PasswordData(password));
+        if (result.isValid()) {
+            return true;
+        }
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(
+                Joiner.on(",").join(validator.getMessages(result)))
+                .addConstraintViolation();
+        return false;
+    }
+}
