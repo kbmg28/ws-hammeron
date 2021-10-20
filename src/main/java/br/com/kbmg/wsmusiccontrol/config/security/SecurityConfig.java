587409d48f680205a1d771a8bd4a8c671778d263
Kevin Gomes <kb.developer.br@gmail.com>
2021-10-20 18:19:49 -0400

    force use https

--- src/main/java/br/com/kbmg/wsmusiccontrol/config/security/SecurityConfig.java
+++ src/main/java/br/com/kbmg/wsmusiccontrol/config/security/SecurityConfig.java
@@ -35,6 +35,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .requiresChannel()
+            .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+            .requiresSecure()
+            .and()
             .cors().and().csrf().disable()
             .headers().cacheControl().disable()
             .and()
