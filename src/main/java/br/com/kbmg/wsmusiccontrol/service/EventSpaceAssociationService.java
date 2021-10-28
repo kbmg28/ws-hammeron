89dfd5818d73da0546fef4f0042d6f8e45ebcb58
Kevin Gomes <kb.developer.br@gmail.com>
2021-10-27 20:01:31 -0400

    remove EventSpaceAssociation

--- src/main/java/br/com/kbmg/wsmusiccontrol/service/EventSpaceAssociationService.java
+++
@@ -1,6 +0,0 @@
-package br.com.kbmg.wsmusiccontrol.service;
-
-import br.com.kbmg.wsmusiccontrol.model.EventSpaceAssociation;
-
-public interface EventSpaceAssociationService extends GenericService<EventSpaceAssociation>{
-}
