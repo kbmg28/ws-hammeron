a33fb7a6eeaf733f94e7d98ef336144812d1cbb6
Kevin Gomes <kb.developer.br@gmail.com>
2021-12-09 22:28:17 -0400

    Only Space Owner can change status rejected

--- src/main/java/br/com/kbmg/wsmusiccontrol/constants/KeyMessageConstants.java
+++ src/main/java/br/com/kbmg/wsmusiccontrol/constants/KeyMessageConstants.java
@@ -23,4 +23,5 @@ public abstract class KeyMessageConstants {
     public static final String USER_EMAIL_NOT_EXISTS = "user.email.not.exists";
 
     public static final String MUSIC_INVALID_LINK = "music.invalid.link";
+    public static final String MUSIC_CANNOT_CHANGE_STATUS = "music.cannot.change.status";
 }
